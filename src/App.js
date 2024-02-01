8738458d27cab979b5d43b0326c506213362a2ff
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-02-01 16:47:30 -0500

    Add freeCodeCamp logo to the web app.
    Add the logo image to the 'src/images' folder.
    Add a <div> section to the logo in the 'App.js' file.
    Add styles to 'App'.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import freeCodeCampLogo from './images/freecodecamp-logo.png'
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='freecodecamp-logo-wrapper'>
+        <img
+          className='freecodecamp-logo'
+          src={freeCodeCampLogo}
+          alt='FreeCodeCamp logo'
+        />
+      </div>
     </div>
   );
 }
