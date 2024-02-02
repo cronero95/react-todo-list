9d4cc90ff900152357623dc5fb6a3a9d4efd4733
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-02-01 21:40:09 -0500

    Add 'react-icons' package and use the delete one.
    Since the 'react-icons' package brings the option to add a 'className' to his components, it's used to assing styles to the icon.

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../stile-sheets/ToDo.css'
+import { AiOutlineCloseCircle } from 'react-icons/ai'
 
 function ToDo( {text} ) {
   return (
@@ -7,8 +8,10 @@ function ToDo( {text} ) {
       <div className='todo-text'>
         {text}
       </div>
-      <div className='todo-icon'>
-        Delete
+      <div className='todo-icon-wrapper'>
+        {/* It's possible to assign a className because of the package configuration, 
+        in other components this is just a prop, so won't work. */}
+        <AiOutlineCloseCircle className='todo-icon' />
       </div>
     </div>
   );
