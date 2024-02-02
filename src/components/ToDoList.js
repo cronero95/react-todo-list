47d9da6d5bee177c8c0f94631edeba4e8c8502b1
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-02-02 00:09:09 -0500

    Add functionalities to 'ToDoList' component.
    Add the functionalities to add and delete 'ToDo' components.
    Add the functionality to mark as finished a 'ToDo'.
    Modify some styles to mantain the good looking of the web app.

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,13 +1,53 @@
 import React from 'react';
+import { useState } from 'react';
 import '../stile-sheets/ToDoList.css'
 import ToDoForm from './ToDoForm';
+import ToDo from './ToDo';
 
 function ToDoList() {
+
+  const [toDoList, setToDoList] = useState([]);
+
+  const addToDo = (toDo) => {
+    if(toDo.text.trim()) {
+      toDo.text = toDo.text.trim();
+      
+      const toDoListUpdated = [toDo, ...toDoList];
+      setToDoList(toDoListUpdated);
+    }
+  };
+
+  const deleteToDo = (id) => {
+    const toDoListUpdated = toDoList.filter(toDo => toDo.id !== id);
+    setToDoList(toDoListUpdated);
+  };
+
+  const finishToDo = (id) => {
+    const toDoListUpdated = toDoList.map(toDo => {
+      if (toDo.id == id) {
+        toDo.finished = !toDo.finished;
+      }
+      return toDo;
+    });
+    setToDoList(toDoListUpdated);
+  };
+
   return (
     <>
-      <ToDoForm/>
-      <div className='todo-list-wrapper'>
-        TODO LIST
+      <ToDoForm onSubmit={addToDo}/>
+      <div className='list-wrapper'>
+        {
+          toDoList.map((toDo) => 
+            <ToDo
+              key={toDo.id}
+              id={toDo.id}
+              text={toDo.text}
+              finished={toDo.finished}
+              deleteToDo={deleteToDo}
+              finishToDo={finishToDo}
+            />
+          )
+        }
       </div>
     </>
   );
