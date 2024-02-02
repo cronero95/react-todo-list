aa06383176099135c745a9546e541dcd4ebc868b
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-02-01 23:32:15 -0500

    Add functions to send a 'ToDo'.

--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -1,16 +1,43 @@
 import React from 'react';
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid'
 import '../stile-sheets/ToDoForm.css'
 
 function ToDoForm(props) {
+
+  const [input, setInput] = useState('');
+
+  const changeManagement = (e) => {
+    setInput(e.target.value);
+  };
+
+  const sendManagement = (e) => {
+    e.preventDefault();
+
+    const newToDo = {
+      id: uuidv4(),
+      text: input,
+      finished: false
+    };
+
+    props.onSubmit(newToDo);
+  };
+
   return (
-    <form className='todo-form'>
+    <form 
+      className='todo-form'
+      onSubmit={sendManagement}
+    >
       <input
         className='todo-input'
         type='text'
         placeholder='Write a ToDo'
         name='text'
+        onChange={changeManagement}
       />
-      <button className='todo-button'>
+      <button 
+        className='todo-button'
+      >
         Add ToDo
       </button>
     </form>
