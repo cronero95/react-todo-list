e72a652a5bf0343802065746d17ec160ad87e5d5
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-02-01 22:57:08 -0500

    Add new props to 'ToDo' component.
    Add 'id' prop to identify each new 'ToDo'.
    Add 'finishToDo' to call a function which marks as finished a 'ToDo' when the text is pressed.
    Add 'deleteToDo' to call a function which deletes a 'ToDo' when the delete icon is pressed.

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -2,16 +2,22 @@ import React from 'react';
 import '../stile-sheets/ToDo.css'
 import { AiOutlineCloseCircle } from 'react-icons/ai'
 
-function ToDo( {text, finished} ) {
+function ToDo( {id, text, finished, finishToDo, deleteToDo} ) {
   return (
     <div className={finished ? 'todo-wrapper finished' : 'todo-wrapper'}>
-      <div className='todo-text'>
+      <div 
+        className='todo-text'
+        onClick={() => finishToDo(id)}
+      >
         {text}
       </div>
       <div className='todo-icon-wrapper'>
         {/* It's possible to assign a className because of the package configuration, 
         in other components this is just a prop, so won't work. */}
-        <AiOutlineCloseCircle className='todo-icon' />
+        <AiOutlineCloseCircle 
+          className='todo-icon'
+          onClick={() => deleteToDo(id)}
+        />
       </div>
     </div>
   );
